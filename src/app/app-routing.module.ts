5cf8758fd6e26b4d4102a791c82efa783a792907
FrancoPagnotta <fpagnotta@mobydigital.com>
2021-12-27 11:59:03 -0300

    create routes module, shared navbar component and styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { TemplateComponent } from './pages/template/template.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveComponent } from './pages/reactive/reactive.component';
 
-
+const routes: Routes = [
+  { path: 'template', component: TemplateComponent },
+  { path: 'reactive', component: ReactiveComponent },
+  { path: '', component: TemplateComponent },
+  { path: '**', component: TemplateComponent }
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
