1d0114a39124229d8fcd5762512949158aa803fa
FrancoPagnotta <fpagnotta@mobydigital.com>
2022-01-12 00:00:28 -0300

    create Reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './pages/shared/shared.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ReactiveComponent } from './pages/reactive/reactive.component';
 import { TemplateComponent } from './pages/template/template.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -12,15 +12,16 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-  ReactiveComponent,
-  TemplateComponent
+    ReactiveComponent,
+    TemplateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive.component.css']
 })
 export class ReactiveComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { 
+    this.createForm();
+  }
 
   ngOnInit(): void {
+  }
+
+  createForm(): void {
+    this.form = this.formBuilder.group({
+      name: ['',[Validators.required, Validators.minLength(3)]],
+      surename: ['',[Validators.required, Validators.minLength(3)]],
+      email: ['',[Validators.required]]
+    });
+  }
+
+  save(): void {
+    console.log(this.form);
   }
 
 }
